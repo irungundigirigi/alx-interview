f6603287bd744665f8316439ec109fcceeb70585
irungundigirigi <irungundigirigi@gmail.com>
2024-03-11 10:25:18 +0300

    refractor code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,13 @@
-#!/usr/bin/python3
-"""
-    0-pascal_triangle.py
-"""
+#!/usr/bin/env python3
+
 def pascal_triangle(n):
-    """
-    Generates Pascal's triangle up to row n.
-    """
-    if n <= 0:
-        return []
-
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
-
+    triangle = []
+    for i in range(n):
+        t = []
+        for j in range(i+1):
+            if j == 0 or j == i:
+                t.append(1)
+            else:
+                t.append(triangle[i -1][j -1] + triangle[i - 1][j])
+        triangle.append(t)
     return triangle
