0b2c3ca434b70f1bcd540c7151709ef79cc231ca
irungundigirigi <irungundigirigi@gmail.com>
2024-03-20 13:31:54 +0300

    add 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,29 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+'''The minimum operations coding challenge.
+'''
 
-"""
-Module for calculating the minimum number of operations required to reach a certain number of characters.
-"""
 
-def minOperations(n):
-    """
-    Calculates the fewest number of operations needed to result in exactly n H characters in the file.
-    Args:
-        n (int): The desired number of characters.
-
-    Returns:
-        int: The minimum number of operations required.
-    """
-    if n <= 1:
+def min_operations(target_chars):
+    '''Computes the fewest number of operations needed to result
+    in exactly target_chars H characters.
+    '''
+    if not isinstance(target_chars, int):
         return 0
-
-    min_ops = 0
-    divisor = 2
-
-    while n > 1:
-        while n % divisor == 0:
-            min_ops += divisor
-            n //= divisor
-        divisor += 1
-
-    return min_ops
+    operations_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < target_chars:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            operations_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif target_chars - done > 0 and (target_chars - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            operations_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            operations_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return operations_count
 
