0841df13aa7f5ecfc40408e4c5631e8204e0e71b
edwin <irungundigirigi@gmai.com>
2024-04-20 13:10:17 +0300

    get character urls

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const film= 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let  movie_characters = [];
+
+const requestCharacters = async () => {
+  await new Promise(resolve => request(film, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      movie_characters = jsonBody.characters;
+      resolve();
+    }
+  }));
+};
+console.log(people);
