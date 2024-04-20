ebec8d3e71eb2cc14a5a7c45e2b6dcc79d004182
edwin <irungundigirigi@gmai.com>
2024-04-20 13:25:13 +0300

    get character names

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,6 +5,7 @@ const request = require('request');
 const movieId = process.argv[2];
 const film= 'https://swapi-api.hbtn.io/api/films/' + movieId;
 let  movie_characters_urls = [];
+let character_names = [];
 
 const requestCharactersURLs = async () => {
   await new Promise(resolve => request(film, (err, res, body) => {
@@ -17,6 +18,24 @@ const requestCharactersURLs = async () => {
     }
   }));
 };
-
+const getCharacterNames = async () => {
+    if (movie_characters_urls.length > 0) {
+      for (const url of movie_characters_urls) {
+        await new Promise(resolve => request(url, (err, res, body) => {
+          if (err || res.statusCode !== 200) {
+            console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+          } else {
+            const jsonBody = JSON.parse(body);
+            character_names.push(jsonBody.name);
+            resolve();
+          }
+        }));
+      }
+    } else {
+      console.error('Error: Could not get character names.');
+    }
+  };
+  
 requestCharactersURLs()
-console.log(movie_characters_urls);
+getCharacterNames()
+console.log(character_names)
